9d22b83be9f29c4f569297a78162fd0b1e630d50
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-09-16 17:45:50 +0900

    [Silver III] Title: N과 M (3), Time: 348 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,41 @@
+// 15651번 N과 M (3)
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <stack>
+#include <map>
+#include <queue>
+using namespace std;
+
+const int MAX = 8;
+int N, M;
+int pos[MAX];
+
+void dfs(int n, int cnt){
+	if(cnt == M){
+		for(int i = 1; i <= cnt; i++){
+			cout << pos[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+		
+	for(int i = 1; i <= N; i++){
+		cnt += 1;
+		pos[cnt] = i;
+		dfs(i, cnt);
+		
+		cnt -= 1;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N >> M;
+	dfs(0,0);
+	
+	return 0;
+}
