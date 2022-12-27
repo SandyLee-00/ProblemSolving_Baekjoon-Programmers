4ea93d47d28a3fe876c1dab8fb4ba5ad098089ac
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-12-27 19:55:31 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.cpp
+++ 프로그래머스/lv0/120875. 평행/평행.cpp
@@ -1,23 +1,9 @@
 #include <string>
 #include <vector>
-#include <bits/stdc++.h>
 
 using namespace std;
 
 int solution(vector<vector<int>> dots) {
     int answer = 0;
-    map<double, int> m;
-    for(int i = 0; i < 4; i++){
-        for(int j = i+1; j < 4; j++){
-            double dx = dots[i][0] - dots[j][0];
-            double dy = dots[i][1] - dots[j][1];
-            if(dx != 0)
-                m[dy/dx] += 1;
-        }
-    }
-    for(auto it = m.begin(); it != m.end(); it++){
-        if(it->second >= 2)
-            answer = 1;
-    }
     return answer;
 }
