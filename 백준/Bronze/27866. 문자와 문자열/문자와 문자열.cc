43bd9e9341d3ee8c9b61932d9529ff117bad917e
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2023-07-04 21:06:19 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+
+
+int main() 
+{
+	string str;
+	cin >> str;
+	int i;
+	cin >> i;
+	cout << str[i-1];
+	
+	
+	return 0; 
+}
