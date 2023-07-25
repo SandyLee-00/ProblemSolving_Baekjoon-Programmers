97d0c0139fc393420678b1908ee27cc2f0a10bb3
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2023-07-25 16:36:57 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,29 @@
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <utility>
+using namespace std;
+using ll = long long;
+
+/*
+
+
+*/
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	ll N;
+	cin >> N;
+	cout << N * 4;
+	
+	return 0;
+}
