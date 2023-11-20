3d9eeb6faa445029e7ad1b35394ae6495deddb1a
Sandy <syjy8809@gmail.com>
2023-11-20 15:58:00 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,28 @@
+// 최단경로
+/*
+
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <climits>
+#include <set>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int A;
+	cin >> A;
+	int B;
+	cin >> B;
+	cout << A * B;
+
+	
+	return 0;
+}
