e7352a032d561a26c73a19bab567edbc698c28df
Sandy <syjy8809@gmail.com>
2023-11-26 21:33:52 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,43 @@
+// 
+/*
+
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <climits>
+#include <set>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> v(N + 1);
+
+	for(int i = 0; i <= N; i++){
+		v[i] = i;
+	}
+
+	for(int i = 0; i < M; i++){
+		int fir, sec;
+		cin >> fir >> sec;
+		for(int i = fir; i <= (sec + fir) / 2; i++){
+			int tmp = v[i];
+			v[i] = v[sec + fir - i];
+			v[sec + fir - i] = tmp;
+		}
+	}
+
+	for(int i = 1; i <= N; i++){
+		cout << v[i] << " ";
+	}
+	
+	return 0;
+}
