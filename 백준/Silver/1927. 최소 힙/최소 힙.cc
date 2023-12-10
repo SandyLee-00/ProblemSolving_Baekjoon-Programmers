2212ca92b48f9e504d8d2418027682206d24e049
Sandy <syjy8809@gmail.com>
2023-12-10 21:28:06 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,39 +1,48 @@
-// 1927번 최소 힙
+// 
+/*
+
+*/
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <math.h>
-#include <stack>
-#include <map>
 #include <queue>
+#include <stack>
+#include <climits>
+#include <set>
+#include <sstream>
 using namespace std;
+using ll = long long;
 
-int main() {
-	ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	priority_queue<int, vector<int>, greater<int>> h;
-	
-	int N; cin >> N;
-	
-	while(N--){
-		int x; cin >> x;
-		if(x > 0){
-			h.push(x);
-		}
-		else if(x == 0){
-			// 비어있으면 0 출력
-			if(h.empty()) cout << 0 << "\n";
+	int N;
+	cin >> N;
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	for(int i = 0; i < N; i++){
+		int a;
+		cin >> a;
+
+		if(a == 0){
+			if(!pq.empty()){
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
 			else{
-				// 가장 작은 값 출력
-				// 값 제거
-				cout << h.top() << "\n";
-				h.pop();
+				cout << 0 << "\n";
 			}
 			
-			
+		}
+		else{
+			pq.push(a);
 		}
 	}
 	
+
 	return 0;
 }
